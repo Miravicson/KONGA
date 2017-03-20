8e944007f4974659f7a59bb19b65634b76021583
Victor Ughonu <victorughonu95@gmail.com>
2017-03-20 16:26:49 +0100

    modified the settings file

--- KONGA/settings.py
+++ KONGA/settings.py
@@ -60,7 +60,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         #'DIRS': [os.path.join(BASE_DIR, 'templates')]
-	'DIRS': [],        ,
+	'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
 	    'debug': True,
