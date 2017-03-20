3982d7accf43398ed3fe9535c3b21ed588605a23
Victor Ughonu <victorughonu95@gmail.com>
2017-03-20 14:57:58 +0100

    allowed host

--- KONGA/settings.py
+++ KONGA/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'j9*stte(^ejlr!n%a!xu^yy*(_by1rkxll7alb)o8_g*!y+)fg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- KONGA/urls.py
+++ KONGA/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
+admin.autodiscover()
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('webapp.urls')),
